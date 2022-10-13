fcd916d5dcc0e19078bb523bd8508faeb69560e3
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-10-13 09:38:59 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1=8;
+let num2=8;
+let num3=4;
+let num4=2;
+let multiply=num1*num2-2;
+
+
+let random=Math.floor(Math.random()*10)+1;
+
+let mod =num3%num4 +4;
+
+
+let max = Math.max(1,2,3,4,20);
+
